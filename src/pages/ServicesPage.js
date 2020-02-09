4a250e17771cc475c9a334c5d9efbb4a1835fa98
Arnoldo Gonzalez <gonzalezarnoldo8@gmail.com>
2020-02-08 20:41:55 -0800

    Added URL parameters to the Services page

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import servicesContent from './services-content';
 
 const ServicesPage = ({ match }) => {
     const name = match.params.name;
+    const services = servicesContent.find(services => services.name === name);
+
+    if(!services) return <h1>{name} service is not offered.</h1>
 
     return (
         <>
             <h1>Easy Tune Up Mechanic Shop {name} Services</h1>
-            <p>Easy Tune Up has been in business for more than 4 years. We Provide excellent customer service and our staff are highly qualified to handle any mechanical or electrical problem with foreign and domestic vehicles.</p>
+            <h2>{services.title}</h2>
+            {services.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
         </>
     );
 };
---
+++ src/pages/services-content.js
@@ -0,0 +1,76 @@
+const services = [
+    {
+        name: 'Honda',
+        title: 'Honda Transmision replacement',
+        content: [
+            `Welcome! Today we're going to be talking about how to replace your Hondas Transmision
+            proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`
+        ]
+    },
+    {
+        name: 'Toyota',
+        title: 'Toyota Piston replacement',
+        content: [
+            `Welcome! Today we're going to be talking about how to replace your Toyotas Pistons
+            proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`
+        ]
+    },
+    {
+        name: 'Tesla',
+        title: 'Tesla Front Suspension replacement',
+        content: [
+            `Welcome! Today we're going to be talking about how to replace your Teslas Front Suspension
+            proin congue ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`
+        ]
+    },
+];
+
+export default services;
